3b8b62b469dcb2e4cbd46724dab7611792500678
Jaisree146 <jaisreehari1234@gmail.com>
2026-06-30 18:27:34 +0530

    Fix flake8 lint issues

--- task-service/app.py
+++ task-service/app.py
@@ -1,21 +1,27 @@
 from flask import Flask
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
 from config import mysql
 from routes.task_routes import task_bp
-from dotenv import load_dotenv
-from flask_cors import CORS
-import os
+
 load_dotenv()
+
 app = Flask(__name__)
 CORS(app)
+
 app.config["MYSQL_HOST"] = os.getenv("DB_HOST")
 app.config["MYSQL_USER"] = os.getenv("DB_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("DB_PASSWORD")
 app.config["MYSQL_DB"] = os.getenv("DB_NAME")
+
 mysql.init_app(app)
 app.register_blueprint(task_bp)
+
 if __name__ == "__main__":
     app.run(
-    host="0.0.0.0",
-    port=5001,
-    debug=True
-)
+        host="0.0.0.0",
+        port=5001,
+        debug=True,
+    )
--- task-service/middleware/auth_middleware.py
+++ task-service/middleware/auth_middleware.py
@@ -1,24 +1,42 @@
-import jwt
 import os
 from functools import wraps
-from flask import request, jsonify
+
+import jwt
 from dotenv import load_dotenv
+from flask import jsonify, request
+
 load_dotenv()
+
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         auth_header = request.headers.get("Authorization")
+
         if not auth_header:
-            return jsonify({
-                "message":"Token Missing"
-            }), 401
+            return jsonify(
+                {
+                    "message": "Token Missing",
+                }
+            ), 401
+
         token = auth_header.split(" ")[1]
+
         try:
-            payload = jwt.decode(token,os.getenv("JWT_SECRET"), algorithms=["HS256"])
+            payload = jwt.decode(
+                token,
+                os.getenv("JWT_SECRET"),
+                algorithms=["HS256"],
+            )
             request.user = payload
+
         except Exception:
-            return jsonify({
-                "message":"Invalid Token"
-            }), 403
+            return jsonify(
+                {
+                    "message": "Invalid Token",
+                }
+            ), 403
+
         return f(*args, **kwargs)
+
     return decorated
